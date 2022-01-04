9c416b20a349dee7b4f68b5af138fb8a044d7e37
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-01-04 12:38:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+//! O'zgaruvchilar
+
+// console.log("This is index.js")
+
+// var x = 5
+// console.log(x)
+// x = 10
+// var y = 8
+// y = 70
+// var z = x + y
+
+// var x, y, z
+
+// x = 80
+
+// y = 78
+
+// z = 58
+
+// var g = (x + 15) / y * z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// console.log(g)
+
+//! Ma'lumot turlari
+//* String, number, boolean, array, object, undefined, null, NaN
+
+//! String
+// var Ibrohim = "Ibrohim Turaboev 20 96 "
+// var number = "70"
+// var number2 = 2
+// console.log(number2+(+number)) //! ichida nomerlar qabul qilgan stringlar oldidan "+" qo'shilsa uni numberga aylantiradi
+
+// var Mirjalol1 = "Mirjalol Islomov"
+// var Mirjalol2 = 'Mirjalol Islomov'
+// var Mirjalol3 = `Mirjalol Islomov`
+
+// console.log(Mirjalol1)
+// console.log(Mirjalol2)
+// console.log(Mirjalol3)
+
+//! Number
+// var num = 2
+// var num2 = 9.34
+
+//! Boolean => true, false
+// var isOpened = false
+// var isEqual = 10 < 5
+// var string = "Ibrohim" == "ibrohim"
+// var tenglik = "Ibrohim" > "Ibrohi"
+
+// console.log(tenglik)
+
+//! Array 
+// var user = "Ibrohim"
+// var users = [user, "Ismoil", "Mirjalol", "Maqsud", 30, true, null, undefined]
+// console.log(users)
+
+//! Object 
+// var user =
+// {
+//     name: "Mirjalol",
+//     surname: "Islomov",
+//     age: 22,
+//     study: true,
+//     car: true,
+//     isMerried: null,
+//     work: true,
+//     siblings: ["Anvar", "Tohir", "Oypari"],
+//     collegues: {
+//         Ibrohim: {
+//             name: "Ibrohim",
+//             age: 20
+//         },
+//         Maqsud: {
+//             name: "Maqsud",
+//             age: 20
+//         },
+//         Qoymurod: {
+//             name: "Bolta",
+//             age: 98
+//         }
+//     }
+// }
+
+// console.log(user)
+
+//! Undefined
+// var Ibrohim = []
+
+// console.log(Ibrohim[0]) // undefined
+
+//! Null
+// var car = null
+// car = "BMW"
+// console.log(car)
+
+//! NaN => Not a Number
